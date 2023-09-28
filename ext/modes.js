d7272cc6424ba3a6e769d3b3c605fc52849ef3e4
justa-whitefox <justa.redfox2002@gmail.com>
2023-09-28 09:05:08 -0300

    update site state

--- ext/modes.js
+++ ext/modes.js
@@ -1,13 +1,54 @@
 function toggleDarkMode() {
 	const $html = document.documentElement;
-	$html.classList.toggle('dark');
+	$html.classList.toggle('Dark');
 
 	const $img = document.querySelector('#homepage-profile-pic');
 
-	if ($html.classList.contains('dark')) {
+	if ($html.classList.contains('Dark')) {
 		$img.setAttribute('src', '/resources/images/general/homepage_profile_dark.jpg');
 
 	} else {
 		$img.setAttribute('src', '/resources/images/general/homepage_profile.jpg');
 	}
 }
+
+/*---------------------------------------------------------
+ * Translation
+/----------------------------------------------------------*/
+function translate() {
+
+}
+
+/*----------------------------------------------------------
+/ Menu 
+/----------------------------------------------------------*/
+function menuSwift($e) {
+	$e.classList.toggle("Change");	
+
+	let $panel = document.querySelector(".Nav-panel");
+
+	if ($panel.style.display === "inline-block") {
+		$panel.style.display = "none";
+
+	} else {
+		$panel.style.display = "inline-block";
+	}
+
+	if ($panel.style.maxWidth) {
+		$panel.style.maxWidth = null;
+
+	} else {
+		$panel.style.maxWidth = ($panel.scrollWidth * 9) + "%";
+	}
+}
+
+/*
+let $menu = document.getElementsByClassName("MenuButton");
+
+for (let i = 0; i < $menu.length; i++) {
+	$menu[i].addEventListener("click", function() {
+
+		
+	});
+}
+*/
